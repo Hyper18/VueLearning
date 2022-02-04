069760bdb0045264570189b7832636f5b7f3e231
Hyper18 <windhyperspace@gmail.com>
2022-02-04 12:38:11 +0800

    scope样式

---
+++ 25_scope样式/vue_test/src/components/School.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="demo">
+    <h2>名称:{{ name }}</h2>
+    <h2>地址:{{ address }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "School",
+  data() {
+    return {
+      name: "某大",
+      address: "广州",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.demo {
+  background-color: skyblue;
+}
+</style>
