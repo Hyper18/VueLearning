3991118d5d676b14e767e6c72ea2753ca5da7a02
Hyper18 <windhyperspace@gmail.com>
2022-02-04 14:39:57 +0800

    TodoList案例-基础搭建

---
+++ 26_TodoList案例/todo-list/src/components/MyList.vue
@@ -0,0 +1,44 @@
+<template>
+  <ul class="todo-main">
+    <MyItem v-for="todoObj in todos" :key="todoObj.id" :todo="todoObj" />
+  </ul>
+</template>
+
+<script>
+import MyItem from "./MyItem";
+
+export default {
+  name: "MyList",
+  components: {
+    MyItem,
+  },
+  data() {
+    return {
+      todos: [
+        { id: 1, name: "写代码", done: false },
+        { id: 2, name: "学习", done: false },
+        { id: 3, name: "刷剧", done: true },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
