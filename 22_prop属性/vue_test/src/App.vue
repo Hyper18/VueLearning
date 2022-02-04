1c9336c864e02f43da7ca70a9050965819387c75
Hyper18 <windhyperspace@gmail.com>
2022-02-04 11:34:34 +0800

    props属性

---
+++ 22_prop属性/vue_test/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Student name="李四" sex="女" :age="11" />
+    <!-- <hr />
+    <Student />
+    <hr />
+    <Student /> -->
+  </div>
+</template>
+
+<script>
+import Student from "./components/Student";
+
+export default {
+  name: "App",
+  components: { Student },
+};
+</script>
---
+++ 22_prop属性/vue_test/src/components/Student.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <h2>姓名:{{ name }}</h2>
+    <h2>性别:{{ sex }}</h2>
+    <h2>年龄:{{ age }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Student",
+  data() {
+    return {
+      msg: "我是一名学生",
+    };
+  },
+  // props: ["name", "sex", "age"], // 简单声明接收
+
+  /* props: {
+    // 接收同时对数据类型进行限制
+    name: String,
+    sex: String,
+    age: Number,
+  }, */
+  props: {
+    //接收的同时对数据:进行类型限制+默认值的指定+必要性的限制
+    name: {
+      type: String,
+      required: true,
+    },
+    sex: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      default: 18,
+    },
+  },
+};
+</script>
