2dbc3ce5070beafaaf85f0069f6c597f801c5ab6
Hyper18 <windhyperspace@gmail.com>
2022-02-04 11:50:42 +0800

    局部混入与全局混入

---
+++ 23_mixin混入(合)/vue_test/src/components/School.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2 @click="showName">名称:{{ name }}</h2>
+    <h2>地址:{{ address }}</h2>
+  </div>
+</template>
+
+<script>
+//引入一个混合
+import { mixin } from "../mixin";
+
+export default {
+  name: "School",
+  data() {
+    return {
+      name: "某大",
+      address: "广州",
+    };
+  },
+  mixins: [mixin],
+};
+</script>
